e6ac79380331f6c7bf99646ab6de7300371415a0
eugene <eugeneholenstein@gmail.com>
2023-04-25 09:13:16 +1000

    commit tree 4e942126683b31268ec005f5103d32758b8f8397

--- lib/car.rb
+++ lib/car.rb
@@ -1,2 +1,10 @@
 # This loads the code from the vehicle.rb file so we can access its code within this file
-require_relative './vehicle'
+require_relative './vehicle.rb'
+
+class car < Vehicle
+
+    def go
+        "VROOOOM!!!!!!!!!!!!"
+    end
+
+end
